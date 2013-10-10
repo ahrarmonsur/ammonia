164db2349d82ce73aa9d7584fc4ccdc07dbbbdc5
Ahrar Monsur <ahrarmonsur93@gmail.com>
2013-10-09 20:15:03 -0400

    Adds ammonia files to AM-LX

--- crdxform.py
+++ crdxform.py
@@ -1,10 +1,20 @@
 from numpy import matrix, dot, cos, sin, pi
-def rotmat(a1, b1, g1):
-	m1 = matrix([[cos(a1)*cos(b1)*cos(g1) - sin(a1)*sin(g1), sin(a1)*cos(b1)*cos(g1) + cos(a1)*sin(g1), -sin(b1)*cos(g1)],
-				 [-cos(a1)*cos(b1)*sin(g1) - sin(a1)*cos(g1), -sin(a1)*cos(b1)*sin(g1) + cos(a1)*cos(g1), sin(b1)*sin(g1)],
-				 [cos(a1)*sin(b1), sin(a1)*sin(b1), cos(b1)]])
-	vec = matrix([[1],[0],[0]])
-	return m1*vec
+def rotmat(a, b, g):
+	R_a = matrix([[cos(a), -sin(a), 0],
+				  [sin(a), cos(a), 0],
+				  [0, 0, 1]])
+	R_b = matrix([[cos(b), 0, sin(b)],
+				  [0, 1, 0],
+				  [-sin(b), 0, cos(b)]])
+	R_g = matrix([[cos(g), -sin(g), 0],
+				  [sin(g), cos(g), 0],
+				  [0, 0, 1]])
+	# m1 = matrix([[cos(a)*cos(b)*cos(g) - sin(a)*sin(g), sin(a)*cos(b)*cos(g) + cos(a)*sin(g), -sin(b)*cos(g)],
+	# 			 [-cos(a)*cos(b)*sin(g) - sin(a)*cos(g), -sin(a)*cos(b)*sin(g) + cos(a)*cos(g), sin(b)*sin(g)],
+	# 			 [cos(a)*sin(b), sin(a)*sin(b), cos(b)]])
+	m = R_a*R_b*R_g
+	return m
 
-print rotmat(-pi/2.0,0.0,0.0)
-#print dir(np)
+
+
+# print rotmat(pi/2.0,pi/4.0,pi/8.0)
